f3fcf900626c6771a89d4e0d9979c599fad5ec54
UlianaShpineva <43916971+UlianaShpineva@users.noreply.github.com>
2022-12-25 18:01:46 +0300

    Update Story.java

--- lab3/lab3/Story.java
+++ lab3/lab3/Story.java
@@ -2,21 +2,12 @@ package lab3;
 
 public class Story {
     private final Neznayka neznayka;
-    private final Rat rat1;
-    private final Rat rat2;
 
     public Story() {
         neznayka = new Neznayka("Neznayka");
-        rat1 = new Rat("Rat1");
-        rat2 = new Rat("Rat2");
     }
 
     public void go() {
-        rat1.run();
-        rat1.stop();
-        rat1.glance(neznayka);
-        rat2.run();
-        rat2.moveNose();
         neznayka.memorise("days", 10);
         neznayka.memorise("people", 48);
         neznayka.memorise("days_in_month", 30);
