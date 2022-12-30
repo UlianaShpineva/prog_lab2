88e90d00bf174784e421ebd71a00228a6f306d41
UlianaShpineva <43916971+UlianaShpineva@users.noreply.github.com>
2022-12-30 17:22:47 +0300

    Update Main.java

--- lab4/Main.java
+++ lab4/Main.java
@@ -1,7 +1,7 @@
 import lab4.*;
 
 public class Main {
-    public static void main (String[] args) throws Exception {
+    public static void main (String[] args) {
         Story story = new Story();
         story.go();
     }
